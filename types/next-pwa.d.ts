3dfb1f686fc76dc159cf1b63a67ecaf7d074a78a
Somesh2504 <someshchevula25@gmail.com>
2026-01-01 12:02:04 +0530

    deployment errors

---
+++ types/next-pwa.d.ts
@@ -0,0 +1,15 @@
+declare module "next-pwa" {
+    import type { NextConfig } from "next";
+  
+    type PWAOptions = {
+      dest?: string;
+      register?: boolean;
+      skipWaiting?: boolean;
+      disable?: boolean;
+    };
+  
+    export default function withPWA(
+      config: PWAOptions
+    ): (nextConfig: NextConfig) => NextConfig;
+  }
+  
